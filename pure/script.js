69066e9df19fbd0082de90defb17dec40d30f5bf
huangs0 <59648015+huangs0@users.noreply.github.com>
2023-05-23 00:39:49 +0800

    Update script.js

--- pure/script.js
+++ pure/script.js
@@ -151,7 +151,7 @@ function makePrediction(handLandmarks, faceLandmarks) {
         return "leftEye"
     } else if (distance(cursor, rightEye) < 0.5 * size) {
         return "rightEye"
-    } else if (distance(cursor, nose) < 0.4 * size) {
+    } else if (distance(cursor, nose) < 0.3 * size) {
         return "nose"
     } else if (distance(cursor, mouth) < 0.4 * size) {
         return "mouth"
