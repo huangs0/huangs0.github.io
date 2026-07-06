3c4eabf7eed53e1f716a48998c3b0f876945a5e7
Huang Songlin <huangs0@connect.hku.hk>
2026-07-06 18:25:21 +0000

    update

--- src/app/config.ts
+++ src/app/config.ts
@@ -18,15 +18,10 @@ export const basicInfo = {
   resumeUrl: "/resume.pdf",
 };
 
-export const summaryInfo = `I am a MPhil student in [HKU AIoT Lab](https://aiot.hku.hk) supervised by [Prof. Wu Chenshu](https://www.cswu.me) and [Prof. Wu Chuan](https://i.cs.hku.hk/~cwu/). 
-Before that, I got my Bachelor of Engineering in Computer Science from the University of Hong Kong with First Class Honours. 
-I also obtain several extensive industrial experience in NVIDIA, miHoYo, etc.
-
-My current research interest spans across GPU-Related Operating and Networking System, especially the intra-gpu-kernel level performance engineering.
-Previously I was also interested in mobile computing, human-computer interaction, web development, computer vision, etc.`
+export const summaryInfo = `I am a Graduate Software Engineer at IMC Trading HK. Before that, I got my MPhil in Computer Science with top 5% and Bachelor of Engineering in Computer Science with First Class Honours from the University of Hong Kong. I worked in Operating System and Networking.`
 
 export const newsInfo: string[] = [
-  `[Mar 2025] 🥳🥳 [**NEUTRINO: Fine-grained GPU Kernel Profiling via Programmable Probing**](https://www.usenix.org/conference/osdi25/presentation/huang-songlin) is **ACCEPTED** at **OSDI'25**!!! This is my first accepted research paper 🥹🥹 after six years' study of computer science.`,
+  `[Mar 2025] 🥳🥳 [**Neutrino**](https://www.usenix.org/conference/osdi25/presentation/huang-songlin) is **ACCEPTED** at **OSDI'25**!!! This is my first accepted research paper 🥹🥹 after six years' study of computer science.`,
   `[Sep 2023] 🎉🎉 My first official tutorship for the course COMP3230 Principles of Operating System`,
   `[July 2023] 🥳🥳 Join miHoYo as a research intern on digital avatar and gaze estimation~`,
   `[Jun 2023] 🎉🎉 I graduate from HKU as a B.Eng. in Computer Science with First Class Honor!`,
@@ -40,45 +35,45 @@ export const awardsInfo: string[] = [
 
 export const vitaeInfo: VitaeProps[] = [
   {
+    orgName: "Netpreme Corporation",
+    positionTitle: "",
+    timespan: "Feb 2026-Jun 2026",
+    description: "",
+    right: false,
+  },
+  {
     orgName: "miHoYo",
     positionTitle: "Research Intern",
     timespan: "Jun - Aug 2023",
-    description: "Manager: Mr. Wenping Zhao",
+    description: "",
     right: false,
   }, 
   {
     orgName: "HKU",
     positionTitle: "M.Phil. in Computer Science",
     timespan: "2023 - 2025",
-    description: "Supervisor: Dr. Chenshu Wu",
+    description: "",
     right: true,
   }, 
   {
     orgName: "HKU",
     positionTitle: "Research Assistant",
     timespan: "Oct 2022 - Apr 2023",
-    description: "Supervisor: Dr. Chenshu Wu",
-    right: false,
-  }, 
-  {
-    orgName: "NVIDIA",
-    positionTitle: "Machine Learning Platform Architect Intern",
-    timespan: "Jun - Aug 2022",
-    description: "Manager: Mr. Xiaowei Li",
+    description: "",
     right: false,
   }, 
   {
     orgName: "HKU",
     positionTitle: "Teaching Assistant",
     timespan: "Sep - Dec 2021",
-    description: "Instructor: Dr. Tak Wing CHING",
+    description: "",
     right: false,
   }, 
   {
     orgName: "HKU",
     positionTitle: "Research Assistant",
     timespan: "Jun - Aug 2021",
-    description: "Supervisor: Prof. Chuan Wu",
+    description: "",
     right: false,
   }, 
   {
